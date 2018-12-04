b80da2939be7d06fa9b6c0e1ad3e19df73832d20
Rebecca Pruim <33818981+rebeccajae@users.noreply.github.com>
2018-12-03 17:54:21 -0800

    Create hrx.py
    This sorta works. It's fulfilling a lot of the example files, It just parses them for now. I'll get to extraction/etc. later. I put them into a dictionary after processing to ensure that they're all accessible. This is certainly more useful for "extraction" but whatever.

---
+++ hrx.py
@@ -0,0 +1,36 @@
+import re
+input_expr = """<===> file1
+This file doesn't have a trailng newline.
+<===> file2
+Neither does this one."""
+
+#So this is kinda hacky buuut it mostly works. I'll work a bit more on it later...
+lines = input_expr.splitlines()
+#We kinda have to assume the first line is a header.
+if len(lines) > 0:
+    firstLine = lines[0]
+    #A little thing is that the first header sentinel is **the** header sentinel.
+    p = re.compile('<=+>')
+    sentinel = p.match(firstLine).group()
+    #Alright, let's strip comments.
+    commentRegex = re.compile(sentinel + '\n[\s\S]*?(?=' + sentinel + ')')
+    input_expr = re.sub(commentRegex, "", input_expr)
+    print(input_expr)
+    #Now, build a regex that matches **all** first lines.
+    headerLine = re.compile(sentinel + " +[^\u0000-\u001F\u007F\u003A\u005C\u000A]+")
+    #Neat. Check it out. This'll tell me the boundaries of the objects.
+    matches = list(re.finditer(headerLine,input_expr))
+    results = {}
+    for i in range(len(matches)):
+        start = matches[i].end()
+        end = len(input_expr)
+        if(i+1 <= len(matches)-1):
+            end = matches[i+1].start()
+        fname = matches[i].group().replace(sentinel, "").strip()
+        fcont = input_expr[start:end].strip('\n')
+        content = {"isDirectory": fname.endswith("/"), "fileContents": fcont}
+
+        results[matches[i].group().replace(sentinel, "").strip()] = content
+    print(results)
+else:
+    print('ERR: No Input.')
