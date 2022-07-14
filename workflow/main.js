f934e41abd783798ec0a815f3bb99481d790f970
Malix <alixbrunetcontact@gmail.com>
2022-07-14 13:29:30 +0200

    Delete main.js

--- workflow/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    app.on(
-        [ "pull_request.opened", "pull_request.edited", "pull_request.synchronize", "pull_request.reopened" ], 
-        async (context) => {
-            
-    });
-};
