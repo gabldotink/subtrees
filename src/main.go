4d6e01afb7214af6a71f965d55f86df8ecb7b49d
DeveloperC <DeveloperC@protonmail.com>
2022-10-12 22:13:44 +0100

    feat: query Accounts API and print result

--- src/main.go
+++ src/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+
+	if accessToken == "" {
+		log.Fatalln("ACCESS_TOKEN is not set.")
+	}
+
+	baseUrl := "https://api-sandbox.starlingbank.com/api/v2"
+
+	client := http.Client{}
+	request, err := http.NewRequest("GET", baseUrl+"/accounts", nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request.Header = http.Header{
+		"Content-Type":  {"application/json"},
+		"Authorization": {"Bearer " + accessToken},
+	}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf(string(body))
 }
