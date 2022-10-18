6a12832abeea5a0b5659d9a2b30757b7f01a7802
DeveloperC <DeveloperC@protonmail.com>
2022-10-18 16:01:37 +0100

    refactor: adding log level setting logs

--- cmd/starling-bank-technical-challenge/main.go
+++ cmd/starling-bank-technical-challenge/main.go
@@ -12,25 +12,37 @@ import (
 )
 
 func main() {
-	// Parse and set the logging level either via an environmetn variable or CLI parameter.
-	cliLogLevel := flag.String("log-level", "", "The level of to display. Can also be set by the environment variable 'LOG_LEVEL'. Defaults to the value 'info'.")
+	defaultLogLevel := "info"
+	envLogLevelName := "LOG_LEVEL"
+
+	// Reading in log level from the CLI.
+	cliLogLevel := flag.String("log-level", "", "The level of to display. Can also be set by the environment variable '"+envLogLevelName+"'. Defaults to the value '"+defaultLogLevel+"'.")
 	flag.Parse()
 
-	logLevel := "info"
+	// Reading in log level from env.
+	envLogLevel, envLogLevelSet := os.LookupEnv(envLogLevelName)
 
-	// CLI provided takes precedence and then enviroment variable.
+	logLevel := ""
+
+	// If you specify an option by using a parameter on the command line, it overrides any value from the corresponding environment variable.
 	if *cliLogLevel != "" {
+		log.Info("Setting the log level from the command line parameter.")
 		logLevel = *cliLogLevel
-	} else if envLogLevel, envLogLevelSet := os.LookupEnv("LOG_LEVEL"); envLogLevelSet {
+	} else if envLogLevelSet {
+		log.Info("Setting the log level from the environment variable.")
 		logLevel = envLogLevel
+	} else {
+		log.Info("Setting the log level from the default.")
+		logLevel = defaultLogLevel
 	}
 
 	parsedLogLevel, err := log.ParseLevel(logLevel)
 
 	if err != nil {
-		log.WithError(err).Fatalf("Failed to parse %q to a log level, the lalid log levels are %q.", logLevel, log.AllLevels)
+		log.WithError(err).Fatalf("Failed to parse %q to a log level, the valid log levels are %q.", logLevel, log.AllLevels)
 	}
 
+	log.Infof("Setting the log level to %q.", logLevel)
 	log.SetLevel(parsedLogLevel)
 
 	log.SetFormatter(&log.TextFormatter{
