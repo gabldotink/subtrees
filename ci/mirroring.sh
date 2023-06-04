1c3bce25f494a67b2012808b3c697b9de285a798
DeveloperC <DeveloperC@protonmail.com>
2023-06-04 22:58:45 +0100

    ci: correctly mirroring commits

--- ci/mirroring.sh
+++ ci/mirroring.sh
@@ -5,5 +5,7 @@ set -o nounset
 
 # Setup Git mirror.
 git remote set-url origin "https://oauth2:${GITLAB_PERSONAL_ACCESS_TOKEN}@gitlab.com/DeveloperC/starlingbanktechnicalchallenge"
-# Push commits and tags to Git mirror.
+# Push commits to Git mirror.
+git push --force
+# Push tags to Git mirror.
 git push --tags --force
